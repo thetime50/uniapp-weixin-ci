e4e27ffdf60e00deac3a02ab7f0e5366b91b8092
thetime50 <1004763777@qq.com>
2021-10-06 23:22:24 +0800

    ignore annotate message branch 参数配置

--- src/index.js
+++ src/index.js
@@ -46,20 +46,125 @@ const resolve = path.resolve
  * 参数配置 
  */
 
+// 文件目录检查
+// 插入package script 命令
+
 class APP_CFG_C {
-    APP_PLATFORM = 'mp-weixin'
-    APP_VERSION = '0.0.0'
-    APP_ERR_PATH = resolve('./outinfo')
-    APP_SOURCEMAP_PATH = resolve('./outinfo')
-    APP_SRC_PATH = resolve('./src')
-    APP_PROJECT_PATH = resolve('./dist/dev/mp-weixin')
-    APP_PRIVATE_KEY_PATH = resolve('./keys/wx-private.key')
-    APP_GIT_BRANCH = ['main','master']
+    constructor(){
+        this.APP_PLATFORM = 'mp-weixin'
+        this.APP_VERSION = '0.0.0'
+        this.APP_MESSAGE = '更新'
+        this.APP_ERR_PATH = resolve('./outinfo')
+        this.APP_SOURCEMAP_PATH = resolve('./outinfo')
+        this.APP_SRC_PATH = resolve('./src')
+        this.APP_PROJECT_PATH = resolve('./dist/dev/mp-weixin')
+        this.APP_PRIVATE_KEY_PATH = resolve('./keys/wx-private.key')
+        this.APP_GIT_BRANCH = ['main','master']
+    }
 }
 
 // todo create dir
 
 const APP_CFG = new APP_CFG_C
+
+/**
+ * 命令行参数
+ */
+let appOptions = {}
+const ignoreInfoList = {
+        git:'git: 跳过所有git操作',
+        gb:'gb: 跳过git branch 检查',
+        gf:'gf: 跳过git fetch origin',
+        gd:'gd: 跳过git diff',
+        gt:'gt: 跳过git tag',
+    
+        no:'npm-outdated/no: 跳过npm outdated依赖更新检查',
+    
+        b:'build/b: 跳过npm run build:mp-weixin 编译小程序',
+    
+        ci:'ci: 跳过所有ci操作 (上传 下载sourceMap)',
+        cs:'cs: 跳过下载sourceMap',
+}
+function argvOptionParse(){
+    const ignoreOpts = [ // 内部检查都使用缩写
+        'git',
+        'gb',
+        'gf',
+        'gd',
+        'gt',
+        'npm-outdated',
+        'no',
+        'build',
+        'b',
+        'ci',
+        'cs',
+    ]
+    const ignoreInfo = `跳过的操作步骤:
+        ${ignoreInfoList.git}
+        ${ignoreInfoList.gb}
+        ${ignoreInfoList.gf}
+        ${ignoreInfoList.gd}
+        ${ignoreInfoList.gt}
+
+        ${ignoreInfoList.no}
+
+        ${ignoreInfoList.b}
+
+        ${ignoreInfoList.ci}
+        ${ignoreInfoList.cs}
+    `
+    function parseIgnore(value,previous){
+
+        previous[value] = true
+        if(value == 'npm-outdated'){
+            previous.no = true
+        }
+        if(value == 'build'){
+            previous.b = true
+        }
+        return previous
+    }
+    const pkg = require('../package')
+    program
+        .version(`${pkg.name} ${pkg.version}`)
+        .option('-i, --ignore <type...>', ignoreInfo,parseIgnore,{})
+        .option('-a, --annotate <string>', `tag 名称 版本号`,'') // 解析完直接更新到APP_CFG上去
+        .option('-m, --message <string>', `版本更新信息`,'')
+        .option('-b, --branch <string>', '选择分支','')
+        .parse(process.argv)
+    appOptions = program.opts();
+
+    const {ignore} = appOptions
+
+    const errIgnore = Object.keys( ignore).filter((v,i,a)=>{
+        return !ignoreOpts.includes(v)
+    })
+
+    if(errIgnore.length){
+        throw new Error(`不支持的 ignore 参数: ${errIgnore.join(' ')} 执行 npx wxci-publish -h 查看说明`)
+    }
+    if(appOptions.annotate && !appOptions.message ||
+        !appOptions.annotate && appOptions.message){
+            throw new Error('请同时设置 annotate 和 message 参数')
+        }
+    if(appOptions.annotate){
+        APP_CFG.APP_VERSION = appOptions.annotate
+    }
+    if(appOptions.message){
+        this.APP_MESSAGE = appOptions.message
+    }
+    if(appOptions.branch){
+        APP_CFG.APP_GIT_BRANCH = appOptions.branch
+    }
+}
+
+function ignoreCheck(flag){ // 如果返回true 跳过执行 
+    const res = appOptions.ignore[flag]
+    if(res){
+        console.log(`ignore ${ignoreInfoList[flag]}`,logSymbols.info)
+    }
+    return res
+}
 
 const ansiTrimRe = new RegExp('\x1b(?:\\[(?:\\d+[ABCDEFGJKSTm]|\\d+;\\d+[Hfm]|' +
           '\\d+;\\d+;\\d+m|6n|s|u|\\?25[lh])|\\w)', 'g')
@@ -72,6 +177,7 @@ function delay(ms) {
 }
 
 async function gitCheck(){
+    if(ignoreCheck('git')) return
     console.log(`*** git 仓库检查 ***`.blue.bgWhite,)
 
     let spinner
@@ -83,97 +189,132 @@ async function gitCheck(){
 
         // when setting all options in a single object
         const git = simpleGit(process.cwd(), options);
-        // logSymbols
+        
         // 在主分支上
         const branch = await git.branchLocal()
         const branchStr = typeof APP_CFG.APP_GIT_BRANCH  == 'string' ? APP_CFG.APP_GIT_BRANCH : APP_CFG.APP_GIT_BRANCH.join(' or ')
-        spinner = ora(`check branch is ${branchStr}`).start()
-        if(branch.current !== APP_CFG.APP_GIT_BRANCH && !APP_CFG.APP_GIT_BRANCH.includes( branch.current)){
-            let msg = `branch is ${branch.current.cyan} not ${branchStr}`
+        async function gitBranchCheck(){
+            spinner = ora(`check branch is ${branchStr}`).start()
+            if(branch.current !== APP_CFG.APP_GIT_BRANCH && !APP_CFG.APP_GIT_BRANCH.includes( branch.current)){
+                let msg = `branch is ${branch.current.cyan} not ${branchStr}`
+                spinner.stop()
+                console.log(msg,logSymbols.error)
+                throw new Error(msg)
+            }
             spinner.stop()
-            console.log(msg,logSymbols.error)
-            throw new Error(msg)
+            console.log('branch is '+ branch.current,logSymbols.success)
         }
-        spinner.stop()
-        console.log('branch is '+ branch.current,logSymbols.success)
-
+    
         // 获取更新远端跟踪分支
-        spinner = ora('git fetch origin').start()
-        try{
-            await git.fetch('origin')
-        }catch(e) {
-            let msg = 'git fetch error'
+        async function gitFetchOrigin(){
+            spinner = ora('git fetch origin').start()
+            try{
+                await git.fetch('origin')
+            }catch(e) {
+                let msg = 'git fetch error'
+                spinner.stop()
+                console.log(msg,logSymbols.error)
+                throw new Error(msg)
+            }
             spinner.stop()
-            console.log(msg,logSymbols.error)
-            throw new Error(msg)
+            console.log('git fetch ok',logSymbols.success)
         }
-        spinner.stop()
-        console.log('git fetch ok',logSymbols.success)
 
         // 检查当前分支已提交到远端
-        const diff = await git.diff('remotes/origin/'+branch.current)
-        spinner = ora('check idff').start()
-        if(diff){
-            let msg = `There are file that have not been commit`
+        async function gitDiffCheck(){
+            const diff = await git.diff('remotes/origin/'+branch.current)
+            spinner = ora('check diff').start()
+            if(diff){
+                let msg = `There are file that have not been commit`
+                spinner.stop()
+                console.log(msg,logSymbols.error)
+                throw new Error(msg)
+            }
             spinner.stop()
-            console.log(msg,logSymbols.error)
-            throw new Error(msg)
+            console.log(`diff remotes/origin/${branch.current} ok`,logSymbols.success)
         }
-        spinner.stop()
-        console.log(`diff remotes/origin/${branch.current} ok`,logSymbols.success)
 
         
-        // git tag -n --sort=taggerdate 
-        // const tages = await git.tags() // 按字母顺序排序
-        const tages = await git.tags('-n',{'--sort': '-taggerdate'}) // 按打tag的时间从旧到新排序的
-        // const tages = await git.tags('-n',{'--sort': '-committerdate'}) // 按提交时间从旧到新排序的
-        const lastTag =tages.all.find((v,i,a)=>{
-            // https://docs.npmjs.com/cli/v6/configuring-npm/package-json#version
-            return /^v?(\d+\.)+\d+(-.*)?\b$/.test( v)
-        }) || '0.0.0'
-        let tail = lastTag.replace(/^v?(\d+\.)+(\d+).*/,'$2')
-        tail = Number(tail)+1
-        let newTage = lastTag.replace(/^(v?(\d+\.)+)(\d+).*/,'$1'+tail)
-        let tagParam = await inquirer.prompt([
-            {
-                type: 'input',
-                message: '输入版本号:',
-                name: 'tag',
-                default: newTage, // 默认值
-                validate (val) {
-                    if (tages.all.includes(val)) {
-                        return '版本号重复'
-                    }
-                    return true
+        async function gitTag(){
+            // git tag -n --sort=taggerdate 
+            // const tages = await git.tags() // 按字母顺序排序
+            const tages = await git.tags('-n',{'--sort': '-taggerdate'}) // 按打tag的时间从旧到新排序的
+            // const tages = await git.tags('-n',{'--sort': '-committerdate'}) // 按提交时间从旧到新排序的
+            const lastTag =tages.all.find((v,i,a)=>{
+                // https://docs.npmjs.com/cli/v6/configuring-npm/package-json#version
+                return /^v?(\d+\.)+\d+(-.*)?\b$/.test( v)
+            }) || '0.0.0'
+            let tail = lastTag.replace(/^v?(\d+\.)+(\d+).*/,'$2')
+            tail = Number(tail)+1
+            let newTage = lastTag.replace(/^(v?(\d+\.)+)(\d+).*/,'$1'+tail)
+            let tagParam = await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: '输入版本号:',
+                    name: 'tag',
+                    default: newTage, // 默认值
+                    validate (val) {
+                        if (tages.all.includes(val)) {
+                            return '版本号重复'
+                        }
+                        return true
+                    },
                 },
-            },
-            {
-                type: 'input',
-                message: '输入更新信息：',
-                name: 'message',
-                validate(val){
-                    if(!val){
-                        return '请输入更新信息'
-                    }
-                    return true
-                },
+                {
+                    type: 'input',
+                    message: '输入更新信息：',
+                    name: 'message',
+                    validate(val){
+                        if(!val){
+                            return '请输入更新信息'
+                        }
+                        return true
+                    },
+                }
+            ])
+            
+            spinner = ora('set tag and push remote').start()
+            APP_CFG.APP_VERSION = tagParam.tag
+            await git.tag(['-a',tagParam.tag,'-m',`"${tagParam.message}"`])
+            // tag 提交到远端
+            await git.push('--tags')
+            spinner.stop()
+        }
+        async function gitTagFromOption(){
+            spinner = ora('set tag and push remote').start()
+            // APP_CFG.APP_VERSION = appOptions.annotate // 已经设置过了
+            await git.tag(['-a',APP_CFG.APP_VERSION,'-m',`"${APP_CFG.APP_MESSAGE}"`])
+            // tag 提交到远端
+            await git.push('--tags')
+            spinner.stop()
+        }
+
+        if(!ignoreCheck('gb')){
+            await gitBranchCheck()
+        }
+        if(!ignoreCheck('gf')){
+            await gitFetchOrigin()
+        }
+        if(!ignoreCheck('gd')){
+            await gitDiffCheck()
+        }
+        if(!ignoreCheck('gt')){
+            if(!appOptions.annotate || !appOptions.message){
+                await gitTag()
+            }else{
+                await gitTagFromOption()
             }
-        ])
-        
-        spinner = ora('set tag and push remote').start()
-        APP_CFG.APP_VERSION = tagParam.tag
-        await git.tag(['-a',tagParam.tag,'-m',`"${tagParam.message}"`])
-        // tag 提交到远端
-        await git.push('--tags')
-        spinner.stop()
+        }
     }catch(e){
         // throw e
+        spinner && spinner.stop()
         console.error(e) // message
         process.exit()
     }
 }
 
 async function npmOutdated(){
+    if(ignoreCheck('no')) return
     
     console.log(`*** 依赖更新检查 ***`.blue.bgWhite,)
 
@@ -347,6 +488,7 @@ function parseResponse (stdout) {
 	}
 }
 async function projBuildProcess() {
+    if(ignoreCheck('b')) return
     console.log(`*** 编译微信小程序 ***`.blue.bgWhite,)
     // const spinner = ora('project building...')
     // spinner.start()
@@ -388,10 +530,11 @@ async function projBuildProcess() {
     }) 
     // await delay(3000)
     // spinner.stop()
-    // console.log('')
+    console.log('编译完成')
 }
 
 async function weiXinCi(){
+    if(ignoreCheck('ci')) return
     console.log(`*** 微信小程序ci发布 ***`.blue.bgWhite,)
     const ci = require('miniprogram-ci')
     const platform = requireJSON5(resolve(APP_CFG.APP_SRC_PATH,'manifest.json'))[APP_CFG.APP_PLATFORM]
@@ -407,8 +550,8 @@ async function weiXinCi(){
     console.log(`ci.Project`.blue.bgWhite,project)
     const uploadResult = await ci.upload({
         project,
-        version: '0.0.3',
-        desc: 'hello',
+        version: APP_CFG.APP_VERSION,
+        desc: APP_CFG.APP_MESSAGE,
         setting:{ // https://developers.weixin.qq.com/miniprogram/dev/devtools/ci.html#编译设置
             es6:platform.setting,
             minify:platform.minified,
@@ -416,14 +559,17 @@ async function weiXinCi(){
         onProgressUpdate: console.log,
         robot,
     })
-    console.log(`完成应用代码上传`.blue.bgWhite,uploadResult)
-    const destPath = resolve(APP_CFG.APP_SOURCEMAP_PATH, `source-map-${APP_CFG.APP_VERSION}.zip`) 
-    const sourceMapRes = await ci.getDevSourceMap({
-        project,
-        robot,
-        sourceMapSavePath: destPath
-    })
-    console.log(`sourceMap 路径：`.blue.bgWhite, destPath)
+    
+    if(!ignoreCheck('cs')){
+        console.log(`完成应用代码上传`.blue.bgWhite,uploadResult)
+        const destPath = resolve(APP_CFG.APP_SOURCEMAP_PATH, `source-map-${APP_CFG.APP_VERSION}.zip`) 
+        const sourceMapRes = await ci.getDevSourceMap({
+            project,
+            robot,
+            sourceMapSavePath: destPath
+        })
+        console.log(`sourceMap 路径：`.blue.bgWhite, destPath)
+    }
 }
 
 
@@ -432,6 +578,7 @@ async function weiXinCi(){
  * 
  */
 const methods = {
+    argvOptionParse:argvOptionParse,
     gitCheck:gitCheck,
     npmOutdated:npmOutdated,
     projBuild: projBuildProcess,
@@ -440,9 +587,10 @@ const methods = {
 
 ;
 (async function main(){
+    await methods.argvOptionParse()
     await methods.gitCheck()
     await methods.npmOutdated()
     await methods.projBuild()
     await methods.ci()
-    console.log('应用发布完成'.bold)
+    console.log('应用发布完成'.bold.black.bgWhite)
 })()
